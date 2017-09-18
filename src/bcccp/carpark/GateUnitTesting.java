6fc1d64fb1bfad0f2f47ed8587cbc97b089bcb58
Sunil <sunilthapa5898@gmail.com>
2017-09-18 11:10:58 +1000

    unit testiing for gate lower method in car park entry

--- src/bcccp/carpark/GateUnitTesting.java
+++ src/bcccp/carpark/GateUnitTesting.java
@@ -15,9 +15,13 @@ public class GateUnitTesting {
     public static void main(String[] args){
         System.out.println("creating gate1, an instance of the Gate class...");
         Gate gate1= new Gate(100,100);
-        gate1.raise();
         System.out.println("gate1, an instance of the Gate class created..");
+        System.out.println("Testing the raise method of the Gate class...");
+        gate1.raise();
         System.out.println("Testing the israsied method of the Gate class");
+        System.out.println(gate1.isRaised());
+        System.out.println("Testing the lower method of the Gate class");
+        gate1.lower();
         System.out.println(gate1.isRaised());
         
         
