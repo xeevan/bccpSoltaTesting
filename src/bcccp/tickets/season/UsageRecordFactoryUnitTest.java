8ab6681284e3d2c1e3b078883fe1ff29a1630a58
Iranga Mendis <irangacomz@gmail.com>
2017-09-26 14:25:08 +1000

    Unit Test For Usage Record Factory

---
+++ src/bcccp/tickets/season/UsageRecordFactoryUnitTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bcccp.tickets.season;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author IMM
+ */
+public class UsageRecordFactoryUnitTest {
+    public static void main(String[] args) throws NullPointerException {
+        
+        UsageRecordFactory usageRecordFactory = new UsageRecordFactory();
+        
+         //get current time in millies
+        long millisStart = Calendar.getInstance().getTimeInMillis();
+
+        //Test usageRecordFactory make(method)
+        System.out.println(usageRecordFactory.make("S0001", millisStart).toString());
+        
+        
+    }
+    
+}
